838bee54a86c3d291b62b8bb923961df895992f9
Rudys Natanael Acosta Crousset <natanael926@gmail.com>
2020-08-26 16:42:46 -0400

    Human Friendly Date

--- src/components/PostListItem.vue
+++ src/components/PostListItem.vue
@@ -17,8 +17,9 @@
       </div>
     </div>
 
-    <div class="post-date text-faded">
-      {{post.publishedAt}}
+    <div class="post-date text-faded"
+         :title="post.publishedAt | humanFriendlyDate">
+      {{post.publishedAt | diffForHumans}}
     </div>
 
   </div>
@@ -26,6 +27,7 @@
 
 <script>
   import sourceData from '@/data'
+  import moment from 'moment'
   export default {
     name: 'PostListItem',
     props: {
@@ -40,6 +42,14 @@
       },
       userPostsCount () {
         return Object.keys(this.user.posts).length
+      }
+    },
+    filters: {
+      humanFriendlyDate (date) {
+        return moment.unix(date).format('MMMM Do YYYY, h:mm:ss a')
+      },
+      diffForHumans (date) {
+        return moment.unix(date).fromNow()
       }
     }
   }
