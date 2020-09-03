790ac50347db6c846ae8b38aa9e471fffbec4959
Rudys Natanael Acosta Crousset <natanael926@gmail.com>
2020-09-02 23:04:58 -0400

    Profile page

--- src/components/TheNavbar.vue
+++ src/components/TheNavbar.vue
@@ -20,13 +20,15 @@
       <ul>
 
         <li class="navbar-user">
-          <a href="#">
-            <img class="avatar-small" :src="user.avatar " alt="">
+
+          <router-link :to="{name: 'Profile'}">
+            <img class="avatar-small" :src="user.avatar"
+                 alt="">
             <span>
-                        {{ user.name }}
-                        <img class="icon-profile" src="../assets/img/arrow-profile.svg" alt="">
-                    </span>
-          </a>
+              {{user.name}}
+              <img class="icon-profile" src="../assets/img/arrow-profile.svg" alt="">
+            </span>
+          </router-link>
 
           <!-- dropdown menu -->
           <!-- add class "active-drop" to show the dropdown -->
